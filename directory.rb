978c5da3f37850b3b92196ba087f4718fc488a4d
James <James.Steel@me.com>
2016-09-12 20:20:42 +0100

    added index's to the output

--- directory.rb
+++ directory.rb
@@ -5,8 +5,8 @@ end
 
 
 def print_(students)
-  students.each do |student|
-  puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+  puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
  end
 end
 
