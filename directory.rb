cfc63828498aec407bdd133fc19e1137814641dd
James <James.Steel@me.com>
2016-09-12 19:47:28 +0100

    added methods for ease of reading

--- directory.rb
+++ directory.rb
@@ -1,4 +1,4 @@
-#first we print the list of students
+
 students = [
  "Dr. Hannibal Lecter",
  "Darth Vader",
@@ -12,10 +12,23 @@ students = [
  "Joffrey Baratheon",
  "Norman Bates"
 ]
-puts "The students of Villians Academy"
-puts "--------------------"
-students.each do |student|
-  puts student
-end 
-#finally we print the total number of students
-puts "Overall, we have #{students.count} great students"
+
+def print_header
+  puts "The students of Villians Academy"
+  puts "--------------------"
+end
+
+
+def print_(names)
+  names.each do |name|
+  puts name
+ end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print_(students)
+print_footer(students)
