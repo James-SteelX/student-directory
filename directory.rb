17992e6145fd335054d9316b0a59490fbb94dfde
James <James.Steel@me.com>
2016-09-14 13:34:46 +0100

    refactored input, now much shorter

--- directory.rb
+++ directory.rb
@@ -42,70 +42,74 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
 def input_students
- students = @students
- puts "Whats is the students name?".center(20)
- villian = gets.strip
-
- puts "What cohort are they part of?".center(20)
- month = gets.capitalize.strip
-
- puts "Would you like to add more information?".center(20)
- answer = gets.downcase.strip
-
-  while !villian.empty? || !month.empty? do
-    if
-      @months.include?(month.to_sym)
-      cohort = month
-    else
-      cohort = :May
-    end
-    if villian == ""
-      name = @default_name
-    else
-      name = villian.capitalize
-    end
-    if answer == "yes"
-      puts "Do they have a hobby?".center(20)
-      hobby = gets.chomp.capitalize
-
-      puts "How all are they in cms?".center(20)
-      height = gets.chomp
-
-      puts "What is there country of birth?".center(20)
-      birth = gets.chomp.capitalize
-    else
-      hobby = "N/A"
-      height = "N/A"
-      birth  = "N/A"
-    end
-
-    students << {name: name, cohort: cohort, height: height, hobby: hobby,
-                 birth: birth,}
-      if students.count == 1
-         puts "Now we have #{students.count} student".center(20)
-      else
-        puts "Now we have #{students.count} students".center(20)
-      end
-      puts "Enter another student or type 'quit' to return to the menu".center(20)
-      villian = gets.chomp
-       if villian == 'quit'
-         break
-       end
-      puts "And what cohort are they part of".center(20)
-      month = gets.capitalize.chomp
-
-      puts "Would you like to add more information?".center(20)
-      answer = gets.downcase.strip
-
- end
- students
+   questions
+   while !@villian.empty? || !@month.empty? do
+   checks
+   @students << {name: @name, cohort: @cohort, height: @height, hobby: @hobby,
+                 birth: @birth,}
+    student_counter
+    questions
+   end
+ @students
 end
 
+def questions
+
+ puts "Enter a student name or type quit to return to the menu".center(20)
+ @villian = STDIN.gets.strip
+   if @villian == 'quit'
+     interactive_menu
+   end
+
+ puts "What cohort are they part of?".center(20)
+ @month = STDIN.gets.capitalize.strip
+
+ puts "Would you like to add more information?".center(20)
+ answer = STDIN.gets.downcase.strip
+
+ if answer == "yes"
+  puts "Do they have a hobby?".center(20)
+  hobby = STDIN.gets.chomp.capitalize
+
+  puts "How all are they in cms?".center(20)
+  height = STDIN.gets.chomp
+
+  puts "What is there country of birth?".center(20)
+  birth = STDIN.gets.chomp.capitalize
+ else
+  @hobby = "N/A"
+  @height = "N/A"
+  @birth  = "N/A"
+  end
+end
+
+def checks
+  if
+    @months.include?(@month.to_sym)
+    @cohort = @month
+  else
+    @cohort = :May
+  end
+  if @villian == ""
+    @name = @default_name
+  else
+    @name = @villian.capitalize
+  end
+end
+
+def student_counter
+  if @students.count == 1
+     puts "Now we have #{@students.count} student".center(20)
+  else
+    puts "Now we have #{@students.count} students".center(20)
+  end
+
+end
 
 def print_students_list
      if @students.count <= 0
@@ -131,8 +135,20 @@ def save_students
  file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def try_load_students
+   filename = ARGV.first #first argument from the command line
+   return if filename.nil? #get out of method if nothing is given
+   if File.exists?(filename)
+    load_students(filename)
+     puts "Loaded #{@students.count} from #{filename}"
+   else
+     puts "Sorry, #{filename} doesn't exist."
+     exit #quits the program
+   end
+end
+
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
   name, cohort = line.chomp.split(',')
    @students << {name: name, cohort: cohort.to_sym}
