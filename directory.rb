0cba20071b23eeff0b671f318cac86b677012bdb
James <James.Steel@me.com>
2016-09-14 11:53:27 +0100

    started to add save to file

--- directory.rb
+++ directory.rb
@@ -11,6 +11,7 @@ end
 def print_menu
   puts "1. Input Students"
   puts "2. Show students"
+  puts "3. Save the list of students"
   puts "9. Exit"
 end
 
@@ -26,6 +27,8 @@ def process(selection)
     students = input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -112,6 +115,17 @@ def print_students_list
        puts "---------------------------".center(20)
       end
    end
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  #iterates over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+ file.close
 end
 
 def students_by_cohort(students)
